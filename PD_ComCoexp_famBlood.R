ba59a50e7f0fc70d8920698bc499d3c5c06c4cb9
Claire Green <cmgreen1@sheffield.ac.uk>
2018-06-19 16:00:29 +0100

    Update 2018/06/19

--- PD_ComCoexp_famBlood.R
+++ PD_ComCoexp_famBlood.R
@@ -243,3 +243,155 @@ nodetable_celltype <- subset(nodetable_celltype, !(nodetable_celltype$SUID == "N
 
 nodetable_celltype$targetvalPD <- nodetable_celltype$Gene.symbol %in% targetvalPD
 write.csv(nodetable_celltype, "ModifiedPDNodetable.csv", row.names = F)
+
+### Control network ####
+#### Familial Blood ####
+#Read in network nodes
+DEG_PPI <- readLines("/Users/clairegreen/Documents/PhD/Parkinsons/Parkinsons_Code/Results/FamilialBlood/0point3nuggetgenes.txt")
+
+setwd("/users/clairegreen/Documents/PhD/Parkinsons/Parkinsons_Code/Results/GeneExpression/")
+
+DIJ <- read.csv("DIJfilteredresult.csv")
+rownames(DIJ) <- DIJ$Gene.Symbol
+DIJ <- DIJ[,21:28]
+DIJ <- subset(DIJ, rownames(DIJ) %in% DEG_PPI)
+
+FFR <- read.csv("FFRfilteredresult.csv")
+rownames(FFR) <- FFR$Gene.Symbol
+FFR <- FFR[,20:28]
+FFR <- subset(FFR, rownames(FFR) %in% DEG_PPI)
+
+MID1 <- read.csv("MID1filteredresult.csv")
+rownames(MID1) <- MID1$Gene.Symbol
+MID1 <- MID1[,20:27]
+MID1 <- subset(MID1, rownames(MID1) %in% DEG_PPI)
+
+MID2 <- read.csv("MID2filteredresult.csv")
+rownames(MID2) <- MID2$Gene.Symbol
+MID2 <- MID2[,21:38]
+MID2 <- subset(MID2, rownames(MID2) %in% DEG_PPI)
+
+MID3 <- read.csv("MID3filteredresult.csv")
+rownames(MID3) <- MID3$Gene.Symbol
+MID3 <- MID3[,21:35]
+MID3 <- subset(MID3, rownames(MID3) %in% DEG_PPI)
+
+MID4 <- read.csv("MID4filteredresult.csv")
+rownames(MID4) <- MID4$Gene.Symbol
+MID4 <- MID4[,21:40]
+MID4 <- subset(MID4, rownames(MID4) %in% DEG_PPI)
+
+MOR.SN <- read.csv("MOR.SNfilteredresult.csv")
+rownames(MOR.SN) <- MOR.SN$Gene.Symbol
+MOR.SN <- MOR.SN[,21:35]
+MOR.SN <- subset(MOR.SN, rownames(MOR.SN) %in% DEG_PPI)
+
+setwd("/users/clairegreen/Documents/PhD/Parkinsons/Parkinsons_Code/Results/FamilialBlood/ControlCoexpression/")
+
+write.csv(DIJ, "DIJ_DEGfilt.csv")
+write.csv(FFR, "FFR_DEGfilt.csv")
+write.csv(MID1, "MID1_DEGfilt.csv")
+write.csv(MID2, "MID2_DEGfilt.csv")
+write.csv(MID3, "MID3_DEGfilt.csv")
+write.csv(MID4, "MID4_DEGfilt.csv")
+write.csv(MOR.SN, "MOR.SN_DEGfilt.csv")
+
+
+#### filter correlations #### BOT/BOT2 not included due to less than 4 samples
+setwd("/Users/clairegreen/Documents/PhD/Parkinsons/Parkinsons_Code/Results/FamilialBlood/ControlCoexpression/")
+
+DIJ <- read.csv("DIJcorresult.csv")
+DIJ$Gene1 <- as.character(lapply(strsplit(as.character(DIJ$X), "\\:"), "[", 2))
+DIJ$Gene2 <- as.character(lapply(strsplit(as.character(DIJ$X), "\\:"), "[", 1))
+DIJ <- DIJ[,c(5,6,1,2,3,4)]
+
+FFR <- read.csv("FFRcorresult.csv")
+FFR$Gene1 <- as.character(lapply(strsplit(as.character(FFR$X), "\\:"), "[", 2))
+FFR$Gene2 <- as.character(lapply(strsplit(as.character(FFR$X), "\\:"), "[", 1))
+FFR <- FFR[,c(5,6,1,2,3,4)]
+FFR$X <- paste(FFR$Gene1,":",FFR$Gene2, sep = "")
+
+MID1 <- read.csv("MID1corresult.csv")
+MID1$Gene1 <- as.character(lapply(strsplit(as.character(MID1$X), "\\:"), "[", 2))
+MID1$Gene2 <- as.character(lapply(strsplit(as.character(MID1$X), "\\:"), "[", 1))
+MID1 <- MID1[,c(5,6,1,2,3,4)]
+
+MID2 <- read.csv("MID2corresult.csv")
+MID2$Gene1 <- as.character(lapply(strsplit(as.character(MID2$X), "\\:"), "[", 2))
+MID2$Gene2 <- as.character(lapply(strsplit(as.character(MID2$X), "\\:"), "[", 1))
+MID2 <- MID2[,c(5,6,1,2,3,4)]
+
+MID3 <- read.csv("MID3corresult.csv")
+MID3$Gene1 <- as.character(lapply(strsplit(as.character(MID3$X), "\\:"), "[", 2))
+MID3$Gene2 <- as.character(lapply(strsplit(as.character(MID3$X), "\\:"), "[", 1))
+MID3 <- MID3[,c(5,6,1,2,3,4)]
+MID3$X <- paste(MID3$Gene1,":",MID3$Gene2, sep = "")
+
+MID4 <- read.csv("MID4corresult.csv")
+MID4$Gene1 <- as.character(lapply(strsplit(as.character(MID4$X), "\\:"), "[", 2))
+MID4$Gene2 <- as.character(lapply(strsplit(as.character(MID4$X), "\\:"), "[", 1))
+MID4 <- MID4[,c(5,6,1,2,3,4)]
+
+MOR.SN<- read.csv("MOR.SNcorresult.csv")
+MOR.SN$Gene1 <- as.character(lapply(strsplit(as.character(MOR.SN$X), "\\:"), "[", 2))
+MOR.SN$Gene2 <- as.character(lapply(strsplit(as.character(MOR.SN$X), "\\:"), "[", 1))
+MOR.SN <- MOR.SN[,c(5,6,1,2,3,4)]
+MOR.SN$X <- paste(MOR.SN$Gene1,":",MOR.SN$Gene2, sep = "")
+
+thresh <- 0.3
+### Filter by r value
+DIJ_cor.5 <- DIJ[DIJ$reg.mat > thresh | DIJ$reg.mat < -thresh,]
+FFR_cor.5 <- FFR[FFR$reg.mat > thresh | FFR$reg.mat < -thresh,]
+MID1_cor.5 <- MID1[MID1$reg.mat > thresh | MID1$reg.mat < -thresh,]
+MID2_cor.5 <- MID2[MID2$reg.mat > thresh | MID2$reg.mat < -thresh,]
+MID3_cor.5 <- MID3[MID3$reg.mat > thresh | MID3$reg.mat < -thresh,]
+MID4_cor.5 <- MID4[MID4$reg.mat > thresh | MID4$reg.mat < -thresh,]
+MOR.SN_cor.5 <- MOR.SN[MOR.SN$reg.mat > thresh | MOR.SN$reg.mat < -thresh,]
+
+
+### Find matches 
+
+Commonedge <- Reduce(intersect, list(DIJ_cor.5$X,FFR_cor.5$X, MID1_cor.5$X, 
+                                     MID2_cor.5$X,MID3_cor.5$X, MID4_cor.5$X,
+                                     MOR.SN_cor.5$X))
+
+
+#Subset each dataset with these common names so they are all the same size
+DIJ_CE <- subset(DIJ_cor.5, DIJ_cor.5$X %in% Commonedge)
+DIJ_CE <- DIJ_CE[order(DIJ_CE$X),]
+FFR_CE <- subset(FFR_cor.5, FFR_cor.5$X %in% Commonedge)
+FFR_CE <- FFR_CE[order(FFR_CE$X),]
+MID1_CE <- subset(MID1_cor.5, MID1_cor.5$X %in% Commonedge)
+MID1_CE <- MID1_CE[order(MID1_CE$X),]
+MID2_CE <- subset(MID2_cor.5, MID2_cor.5$X %in% Commonedge)
+MID2_CE <- MID2_CE[order(MID2_CE$X),]
+MID3_CE <- subset(MID3_cor.5, MID3_cor.5$X %in% Commonedge)
+MID3_CE <- MID3_CE[order(MID3_CE$X),]
+MID4_CE <- subset(MID4_cor.5, MID4_cor.5$X %in% Commonedge)
+MID4_CE <- MID4_CE[order(MID4_CE$X),]
+MOR.SN_CE <- subset(MOR.SN_cor.5, MOR.SN_cor.5$X %in% Commonedge)
+MOR.SN_CE <- MOR.SN_CE[order(MOR.SN_CE$X),]
+
+
+
+CommonGroup <- data.frame(row.names = DIJ_CE$X,
+                          DIJ = DIJ_CE$reg.mat,
+                          FFR = FFR_CE$reg.mat,
+                          MID1 = MID1_CE$reg.mat,
+                          MID2 = MID2_CE$reg.mat, 
+                          MID3 = MID3_CE$reg.mat, 
+                          MID4 = MID4_CE$reg.mat, 
+                          MOR.SN = MOR.SN_CE$reg.mat)
+
+
+CG_conserved_up <- CommonGroup[apply(CommonGroup, MARGIN = 1, function(x) all(x > 0)), ]
+CG_conserved_down <- CommonGroup[apply(CommonGroup, MARGIN = 1, function(x) all(x < 0)), ]
+
+CG_samedir <- rbind(CG_conserved_up, CG_conserved_down)
+CG_samedir$corMean <- rowMeans(CG_samedir, na.rm = FALSE, dims = 1)
+CG_samedir$Gene <- rownames(CG_samedir)
+CG_samedir$Gene1 <- as.character(lapply(strsplit(as.character(CG_samedir$Gene), "\\:"), "[", 2))
+CG_samedir$Gene2 <- as.character(lapply(strsplit(as.character(CG_samedir$Gene), "\\:"), "[", 1))
+
+write.csv(CG_samedir, "PD_0point3.csv", quote = F, row.names = F)
+print(CG_conserved_up)
---
+++ Results/FamilialBlood/ControlCoexpression/pairwisecorr_MOR.SN.R
@@ -0,0 +1,50 @@
+#####MOR.SN####
+uniqueresult <- read.csv("MOR.SN_DEGfilt.csv", row.names = 1)
+
+##For loop for generating regression values and p values
+CorExprMat <- t(uniqueresult)
+
+reg <- matrix(0, ncol(CorExprMat), ncol(CorExprMat))
+p.value <- matrix(0, ncol(CorExprMat), ncol(CorExprMat))
+
+for (i in 1:ncol(CorExprMat)){
+  for (j in 1:ncol(CorExprMat)){
+    reg[i,j] <- cor.test(CorExprMat[,i], CorExprMat[,j], method = "spearman")$estimate
+  }}
+
+rownames(reg) <- colnames(reg) <- colnames(CorExprMat)
+
+for (i in 1:ncol(CorExprMat)){
+  for (j in 1:ncol(CorExprMat)){
+    p.value[i,j] <- cor.test(CorExprMat[,i], CorExprMat[,j], method = "pearson")$p.value
+  }}
+
+rownames(p.value) <- colnames(p.value) <- colnames(CorExprMat)
+
+##Only take upper triangle without diagonal (all comparisons are currently doubled)
+ptri <- p.value
+ptri[lower.tri(ptri, diag = TRUE)] <- NA
+
+#Turn into vector
+library(gdata)
+p.vec <- unmatrix(ptri)
+#Remove NA values
+p.vec <- na.omit(p.vec)
+#Multiple hypothesis testing correction 
+p.adj <- p.adjust(p.vec, method = "fdr", n = length(p.vec))
+
+#Create results table
+reg.mat <- unmatrix(reg)
+reg.mat <- as.data.frame(reg.mat)
+p.adj <- as.data.frame(p.adj)
+p.mat <- as.data.frame(p.vec)
+
+pvals <- merge(p.adj, p.mat, by.x = "row.names", by.y = "row.names")
+rownames(pvals)<- pvals$Row.names
+pvals[,1] <- NULL
+results <- merge(pvals, reg.mat, by.x = "row.names", by.y = "row.names")
+rownames(results)<- results$Row.names
+results[,1] <- NULL
+results <- results[order(results$p.vec),]
+
+write.csv(results, "MOR.SNcorresult.csv")
